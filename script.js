f6e2873c31b0df6b5b6fbc9ffc137bbd17aff335
whilstanp <yohanesadrianevan@gmail.com>
2019-06-03 04:18:32 +0700

    additional scripts

---
+++ script.js
@@ -0,0 +1,11 @@
+onst divInstall = document.getElementById('installContainer');
+const butInstall = document.getElementById('butInstall');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js');
+}
+if (window.location.protocol === 'http:') {
+  const requireHTTPS = document.getElementById('requireHTTPS');
+  const link = requireHTTPS.querySelector('a');
+  link.href = window.location.href.replace('http://', 'https://');
+  requireHTTPS.classList.remove('hidden');
+}
