04f45d3e6b563e94738634d18294dca6de46e8e7
whilstanp <yohanesadrianevan@gmail.com>
2019-06-03 05:29:29 +0700

    service monitoring

--- sw.js
+++ sw.js
@@ -29,3 +29,15 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+self.addEventListener('activate', function(event) {
+  var cacheKeeplist = ['v2'];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
