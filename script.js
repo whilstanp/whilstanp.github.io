81ccb9ed52f2cdd3c4bbd17a71d554614e7b3006
whilstanp <yohanesadrianevan@gmail.com>
2019-06-03 04:32:01 +0700

    additional scripts

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-onst divInstall = document.getElementById('installContainer');
+const divInstall = document.getElementById('installContainer');
 const butInstall = document.getElementById('butInstall');
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js');
