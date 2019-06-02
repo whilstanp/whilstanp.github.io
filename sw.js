e09f97c17592c9439df88817034679ac6ccb8027
whilstanp <yohanesadrianevan@gmail.com>
2019-06-03 05:28:54 +0700

    service monitoring

--- sw.js
+++ sw.js
@@ -14,3 +14,18 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        let responseClone = response.clone();
+        caches.open('v1').then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      });
+    }).catch(function() {
+      return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    })
+  );
+});
