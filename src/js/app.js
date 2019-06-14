e2db71da493b020e59be429f4c37ee0b3ddebfc7
whilstanp <yohanesadrianevan@gmail.com>
2019-06-14 15:27:19 +0700

    core functions

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,56 @@ if ('serviceWorker' in navigator) {
   });
 }
 
+function animate() {
+  title.classList.remove('animate-in');
+  for (var i = 0; i < courseFeatureElements.length; i++) {
+    courseFeatureElements[i].classList.remove('animate-in');
+  }
+  button.classList.remove('animate-in');
+
+  setTimeout(function () {
+    title.classList.add('animate-in');
+  }, 1000);
+
+  setTimeout(function () {
+    courseFeatureElements[0].classList.add('animate-in');
+  }, 3000);
+
+  setTimeout(function () {
+    courseFeatureElements[1].classList.add('animate-in');
+  }, 4500);
+
+  setTimeout(function () {
+    courseFeatureElements[2].classList.add('animate-in');
+  }, 6000);
+
+  setTimeout(function () {
+    courseFeatureElements[3].classList.add('animate-in');
+  }, 7500);
+
+  setTimeout(function () {
+    courseFeatureElements[4].classList.add('animate-in');
+  }, 9000);
+
+  setTimeout(function () {
+    courseFeatureElements[5].classList.add('animate-in');
+  }, 10500);
+
+  setTimeout(function () {
+    courseFeatureElements[6].classList.add('animate-in');
+  }, 12000);
+
+  setTimeout(function () {
+    button.classList.add('animate-in');
+  }, 13500);
+}
+
+animate();
+
+button.addEventListener('click', function() {
+  animate();
+});
+
 body, html {
   margin: 0;
   font-family: Arial, sans-serif;
