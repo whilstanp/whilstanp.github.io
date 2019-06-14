919ec052dd88377ecfbc958297ba022b64ef47b4
whilstanp <yohanesadrianevan@gmail.com>
2019-06-14 09:34:58 +0700

    service monitoring

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', function(event) {
         return response;
       });
     }).catch(function() {
-      return caches.match('/sw-test/gallery/myLittleVader.jpg');
+      return caches.match('/index.html');
     })
   );
 });
