025b835e2f59dc7ae6f5310df4dffddc465f39f7
whilstanp <yohanesadrianevan@gmail.com>
2019-06-14 15:26:40 +0700

    core functions

--- src/js/app.js
+++ src/js/app.js
@@ -15,52 +15,103 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-function animate() {
-  title.classList.remove('animate-in');
-  for (var i = 0; i < courseFeatureElements.length; i++) {
-    courseFeatureElements[i].classList.remove('animate-in');
-  }
-  button.classList.remove('animate-in');
-
-  setTimeout(function () {
-    title.classList.add('animate-in');
-  }, 1000);
-
-  setTimeout(function () {
-    courseFeatureElements[0].classList.add('animate-in');
-  }, 3000);
-
-  setTimeout(function () {
-    courseFeatureElements[1].classList.add('animate-in');
-  }, 4500);
-
-  setTimeout(function () {
-    courseFeatureElements[2].classList.add('animate-in');
-  }, 6000);
-
-  setTimeout(function () {
-    courseFeatureElements[3].classList.add('animate-in');
-  }, 7500);
-
-  setTimeout(function () {
-    courseFeatureElements[4].classList.add('animate-in');
-  }, 9000);
-
-  setTimeout(function () {
-    courseFeatureElements[5].classList.add('animate-in');
-  }, 10500);
-
-  setTimeout(function () {
-    courseFeatureElements[6].classList.add('animate-in');
-  }, 12000);
-
-  setTimeout(function () {
-    button.classList.add('animate-in');
-  }, 13500);
+body, html {
+  margin: 0;
+  font-family: Arial, sans-serif;
 }
 
-animate();
+.animate-in {
+  animation: fade-slide 1.5s forwards;
+}
 
-button.addEventListener('click', function() {
-  animate();
-});
+.title {
+  width: 100%;
+  background-color: #ccc;
+  text-align: center;
+  padding: 30px 0;
+}
+
+.title h1 {
+  margin: 0;
+  color: white;
+  font-size: 1.8em;
+}
+
+.title, .course-feature {
+  opacity: 0;
+}
+
+.content, .start-over {
+  margin: 5px auto;
+  text-align: center;
+  width: 80%;
+  max-width: 500px;
+}
+
+.course-feature {
+  padding: 2px;
+  -webkit-box-shadow: 2px 2px 1px #ccc;
+  -moz-box-shadow: 2px 2px 1px #ccc;
+  box-shadow: 2px 2px 1px #ccc;
+}
+
+.course-feature {
+  margin: 10px auto;
+}
+
+#installable {
+  background-color: #d3ff7b;
+}
+
+#offline {
+  background-color: #9ce9ff;
+}
+
+#responsive {
+  background-color: #ffaf8a;
+}
+
+#push {
+  background-color: #fff395;
+}
+
+#notifications {
+  background-color: #ffcedf;
+}
+
+#native-features {
+  background-color: #ddb5ff;
+}
+
+#more {
+  background-color: #afffc3;
+}
+
+.start-over button {
+  background-color: #fa923f;
+  border: 1px solid #fa923f;
+  -webkit-box-shadow: 2px 2px 2px #ccc;
+  -moz-box-shadow: 2px 2px 2px #ccc;
+  box-shadow: 2px 2px 2px #ccc;
+  padding: 5px;
+  color: white;
+  opacity: 0;
+  cursor: pointer;
+  outline: none;
+}
+
+.start-over button:hover {
+  background-color: #ff7f16;
+}
+
+@keyframes fade-slide {
+  0% {
+    opacity: 0;
+    transform: translateY(50px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
