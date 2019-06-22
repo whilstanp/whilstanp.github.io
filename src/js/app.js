4fc44446e3e11f4d37d6f03677c0cd6564bb43cf
whilstanp <yohanesadrianevan@gmail.com>
2019-06-23 00:35:35 +0700

    core function

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,12 @@
 var title = document.querySelector('.title');
 var courseFeatureElements = document.querySelectorAll('.course-feature');
 var button = document.querySelector('button');
-
 navigator.serviceWorker.register('/sw.js');
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js', {scope: '/'})
   .then(function(reg) {
-    // registration worked
     console.log('Registration succeeded. Scope is ' + reg.scope);
   }).catch(function(error) {
-    // registration failed
     console.log('Registration failed with ' + error);
   });
 }
