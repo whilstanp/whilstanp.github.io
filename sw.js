08bab82240984b3e80b837c12cb26ae17cb242eb
whilstanp <yohanesadrianevan@gmail.com>
2019-06-24 15:09:09 +0700

    service monitoring

--- sw.js
+++ sw.js
@@ -20,30 +20,7 @@ self.addEventListener('install', function(event) {
   );
   return self.clients.claim();
 });
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-      console.log(
-        "[Service Worker] Network request Failed. Serving content from cache: " +
-          error
-      );
-      return caches
-        .open(
-          "sw-precache-v3-sw-precache-webpack-plugin-https://silent-things.surge.sh"
-        )
-        .then(function(cache) {
-          return cache.match(event.request).then(function(matching) {
-            var report =
-              !matching || matching.status == 404
-                ? Promise.reject("no-match")
-                : matching;
-            return report;
-          });
-        });
-    })
-  );
-});
-/*self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(res) {
@@ -82,4 +59,4 @@ self.addEventListener('fetch', function(event) {
         }
       })
   );
-});*/
+});
