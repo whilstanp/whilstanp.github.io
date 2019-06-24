3d3329857e1d7d86151b6937e41de7a98564dee6
whilstanp <yohanesadrianevan@gmail.com>
2019-06-24 10:43:23 +0700

    service monitoring

--- sw.js
+++ sw.js
@@ -3,12 +3,6 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js');
   });
 }
-'use strict';
-var cacheVersion = 1;
-var currentCache = {
-  offline: 'offline-cache' + cacheVersion
-};
-const offlineUrl = 'offline.html';
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('first-app')
@@ -19,31 +13,13 @@ self.addEventListener('install', function(event) {
         '/index.html',
         '/script.js',
         '/sw.js',
-        offlineUrl
+        '/offline.html'
       ])
     })
   );
   return self.clients.claim();
 });
-this.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate' || 
-      (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              // Return the offline page
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        return response || fetch(event.request);
-                    })
-            );
-      }
-});
-/*self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
       .then(function(res) {
@@ -51,13 +27,13 @@ this.addEventListener('fetch', event => {
       })
   );
 });
-self.addEventListener('fetch', function(event) {
+/*self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function() {
       return caches.match(event.request);
     })
   );
-});
+});*/
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
@@ -82,4 +58,4 @@ self.addEventListener('fetch', function(event) {
         }
       })
   );
-});*/
+});
