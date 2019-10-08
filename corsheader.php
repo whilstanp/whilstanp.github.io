f14783c471018e5f3f6754c1db760adebc723ec8
whilstanp <yohanesadrianevan@gmail.com>
2019-10-08 16:06:57 +0700

    backend header embedding

---
+++ corsheader.php
@@ -0,0 +1,7 @@
+<?php
+		header("Access-Control-Allow-Origin: https://krosm-74bc5.firebaseio.com");
+		header("Access-Control-Allow-Credentials: true");
+		header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE");
+		header("Access-Control-Max-Age: 86400");
+		header("Access-Control-Allow-Headers: Content-Type, Accept, X-Requested-With, remember-me");
+?>
